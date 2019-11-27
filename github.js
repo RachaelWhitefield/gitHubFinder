95211391043bc548f65ee1f98e2a1ef688647033
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-11-26 17:44:55 -0700

    added api key and set up async await function to call user

---
+++ github.js
@@ -0,0 +1,16 @@
+class Github {
+    constructor() {
+        this.client_id = "8f33a774072358091fff";
+        this.client_secret = "11e90a0cc65a47601d2f2614199de753ba7d362c";
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profileData = await profileResponse.json();
+
+        return {
+            profile
+        }
+    }
+}
